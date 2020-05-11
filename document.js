7ecf80136c97a9ecebbe1cc9af37751888b208ba
Oksana <oksana.con@mail.ru>
2020-05-11 14:16:58 +0700

    Задание 7, пишшем приложение часть 4

--- document.js
+++ document.js
@@ -1,3 +1,30 @@
+let startBtn = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomevalue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('manthsavings-value')[0],
+    yearSavingsValue = document.getElementsByClassName('yeqrsavings-value')[0],
+
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesBtn = document.getElementsByTagName('button')[0],
+    optionalexpensesBtn = Document.getElementsByTagName('button')[1],
+    countBtn = document.getElementsByTagName('button')[2],
+    optonalExpensesItem = document.querySelectorAll('.optaonalexpenses-item'),
+    incomeItem = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+
+
+
 let money, time;
 
 function start() {
